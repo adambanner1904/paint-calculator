d31eabe66419bebc365f7831030d984392ce8eed
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2023-01-24 17:09:04 +0000

    Created more functions to allow the final script to be nice and clean and added a house and room class

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 from wallpainting import *
-def query_obstruction():
-    w_d = input("Do you have a window or door (1 for yes, 0 for no)")
-    return w_d
-def query_shape_of_obstruction():
-    shape_of_obstruction = input("Is your obstruction square or circular(1 for square, 0 for circular)")
-    return shape_of_obstruction
 
-def create_square_obstruction():
-    obs_h = input("What is the height of your obstruction")
-    obs_w = input("What is the width of your obstruction")
-    wall1.add_space(SquareSpace(obs_h, obs_w))
+house = query_num_rooms_in_house()
 
-def create_circle_obstruction():
-    radius = input("What is the radius of the obstruction")
-    wall1.add_space(CircularSpace(radius))
+for i in range(house.num_of_rooms):
 
-def create_wall():
-    print("What is the height and width of your wall")
-    height = input("Height")
-    width = input("Width")
-    wall = Wall(height, width)
+    query_and_add_room(house)
+    for j in range(house.rooms[i].num_of_walls):
+        pass
 
 
 
--- wallpainting.py
+++ wallpainting.py
@@ -2,12 +2,13 @@ import math
 
 
 # Define a general space could be wall, window or door
-class SquareSpace:
+class RectangularSpace:
 
     # Initialise the space with a height and width
     def __init__(self, height, width):
         self.height = float(height)
         self.width = float(width)
+        self.area = 0
         self.get_area()
 
     # Calculates the area of the space
@@ -28,11 +29,11 @@ class CircularSpace:
 
 # A wall is a more specific definition of a space
 # but inherits from a general space
-class Wall(SquareSpace):
+class Wall(RectangularSpace):
 
     # Initialise with the Space initialisation
     def __init__(self, height, width):
-        SquareSpace.__init__(self, height, width)
+        RectangularSpace.__init__(self, height, width)
 
     # Function to be used when adding a space to
     # recalculate the area
@@ -52,4 +53,100 @@ class Wall(SquareSpace):
     def how_much_paint(self):
         self.min_liters_of_paint = 0.07143 * self.area
         self.max_liters_of_paint = 0.08333 * self.area
+
+
+class Room:
+
+    def __init__(self, name, num_of_walls):
+        self.name = name
+        self.num_of_walls = num_of_walls
+
+
+class House:
+
+    def __init__(self, num_of_rooms):
+        self.num_of_rooms = num_of_rooms
+        self.rooms = []
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+
+def query_num_rooms_in_house():
+    while True:
+        try:
+            num_of_rooms = int(input("How many rooms do you want to paint? "))
+            break
+        except ValueError:
+            print("Please enter a single number as the number of rooms that you want to paint")
+    return House(num_of_rooms)
+
+
+def query_and_add_room(house):
+
+    name_of_room = input("What is the name of the room (Garage, living room, kitchen etc)?")
+
+    while True:
+        try:
+            num_of_walls = int(input("How many walls need to be painted in this room?"))
+            break
+        except ValueError:
+            print("Please enter a whole number of walls that need to be painted.")
+
+    house.add_room(Room(name_of_room, num_of_walls))
+
+
+def create_wall():
+    print("What is the height and width of your wall")
+    height = input("Height")
+    width = input("Width")
+    wall = Wall(height, width)
+    return wall
+
+def query_yes_no(question, default = "yes"):
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n":False}
+    if default is None:
+        prompt = " [y/n]"
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+
+    while True:
+        choice = input(question + prompt).lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with 'yes' or 'no'")
+
+
+def query_obstruction():
+    while True:
+        try:
+            obstruction_yes_no = bool(input("Do you have a window or door (1 for yes, 0 for no)"))
+            break
+        except ValueError:
+            print("Please enter a 1 for yes and a 0 for no.")
+    return obstruction_yes_no
+
+query_obstruction()
+def query_shape_of_obstruction():
+    shape_of_obstruction = input("Is your obstruction square or circular(1 for square, 0 for circular)")
+    return shape_of_obstruction
+
+
+def create_rectangular_obstruction(wall):
+    obstruction_height = input("What is the height of your obstruction")
+    obstruction_width = input("What is the width of your obstruction")
+    wall.add_space(RectangularSpace(obstruction_height, obstruction_width))
+
+
+def create_circle_obstruction(wall):
+    radius = input("What is the radius of the obstruction")
+    wall.add_space(CircularSpace(radius))
+
+
+
 
